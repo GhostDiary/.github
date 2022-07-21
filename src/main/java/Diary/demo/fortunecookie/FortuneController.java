b37ae0352f0491583925584be48d3ddc4e68cb43
Limjongho <whdgh9873@icloud.com>
2022-07-21 15:29:56 +0900

    properties 수정 및 gitignore 테스트

---
+++ src/main/java/Diary/demo/fortunecookie/FortuneController.java
@@ -0,0 +1,15 @@
+package Diary.demo.fortunecookie;
+
+import Diary.demo.config.BaseResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/fortune")
+//public class FortuneController {
+//
+//    @GetMapping("")
+//    public BaseResponse<>
+//
+//}
